8e0603cbad93bf57817662d41cf39ae2843b7634
Junaid Saleem <j.saleem@4dmagic.pk>
2024-03-16 11:40:00 +0500

    Add project files.

---
+++ WebApiMongoDb/Entities/Customer.cs
@@ -0,0 +1,21 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+
+namespace WebApiMongoDb.Entities
+{
+    public class Customer
+    {
+        [BsonId]
+        [BsonElement("_id"), BsonRepresentation(BsonType.ObjectId)]
+        public string? Id { get; set; }
+
+        [BsonElement("customer_name"), BsonRepresentation(BsonType.String)]
+        public string? CustomerName { get; set; }
+
+        [BsonElement("email"), BsonRepresentation(BsonType.String)]
+        public string? Email { get; set; }
+
+
+
+    }
+}
