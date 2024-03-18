87818342dd9c0a6eff0bf444d6532f0798adade0
Junaid Saleem <j.saleem@4dmagic.pk>
2024-03-18 12:06:34 +0500

    Build Secured .NET 8 APIs With Custom JWT Authentication & Authorization using Identity Manager

---
+++ CustomJWTDotNet8AuthWithIdentityManagerSolution/IdentityManagerServerApi/Data/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+namespace IdentityManagerServerApi.Data
+{
+    //public class AppDbContext: IdentityDbContext
+    //{
+    //    public AppDbContext(DbContextOptions options): base(options)
+    //    {
+
+    //    }
+    //}
+
+    public class AppDbContext(DbContextOptions options) : IdentityDbContext<ApplicationUser>(options)
+    {
+
+    }
+}
